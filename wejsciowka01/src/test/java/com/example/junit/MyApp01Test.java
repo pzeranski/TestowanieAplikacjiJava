d8eabdf777c89fe26d9e6d8b42ea7a0439e56ec0
Piotr Zeranski <pzeranski@sigma.ug.edu.pl>
2016-04-04 13:31:04 +0200

    dodaje poprawke wejsciowka01

--- wejsciowka01/src/test/java/com/example/junit/MyApp01Test.java
+++ wejsciowka01/src/test/java/com/example/junit/MyApp01Test.java
@@ -23,20 +23,48 @@ public class MyApp01Test {
 		mock = createMock(IHouseManager.class);
 		myApp = new MyApp(mock);
 	}
-
 	@Test
-	public void addingCheck() {
-		
+	public void addingCheck()
+	{
 		House domek = new House("Ulica", true, 1);
 		List<House> houses = new ArrayList<House>();
 		houses.add(domek);
-		
-		
 		expect(mock.addHouse(domek)).andReturn(true).atLeastOnce();
-				
+		replay(mock);
+		assertEquals(true, myApp.addHouse(domek));
+		verify(mock);
+	}
+	@Test
+	public void removingCheck()
+	{
+		House domek = new House("Ulica", true, 1);
+		List<House> houses = new ArrayList<House>();
+		houses.add(domek);
 		expect(mock.removeHouse(domek)).andReturn(true).atLeastOnce();
-		
+		replay(mock);
+		assertEquals(true, myApp.removeHouse(domek));
+		verify(mock);
+	}	
+	@Test
+	public void getAllCheck()
+	{
+		House domek = new House("Ulica", true, 1);
+		List<House> houses = new ArrayList<House>();
+		houses.add(domek);
 		expect(mock.getAll()).andReturn(houses).atLeastOnce();
+		replay(mock);
+		assertEquals("Ulica", myApp.getAll().get(0).getUlica());
+		verify(mock);
+	}		
+	
+	@Test
+	public void findByXCheck() {
+		
+		House domek = new House("Ulica", true, 1);
+		House domek2 = new House("Ulica2", true, 2);
+		List<House> houses = new ArrayList<House>();
+		houses.add(domek);
+		houses.add(domek2);
 		
 		expect(mock.findbyUlica("Ulica")).andReturn(true).atLeastOnce();
 
@@ -44,11 +72,8 @@ public class MyApp01Test {
 		
 		replay(mock);
 		
-		assertEquals(true, myApp.addHouse(domek));
 		assertEquals(true, myApp.findbyUlica("Ulica"));
-		assertEquals("Ulica", myApp.getAll().get(0).getUlica());
 		assertEquals(true, myApp.findbyNumer(1));
-		assertEquals(true, myApp.removeHouse(domek));
 		verify(mock);
 	}
 }
